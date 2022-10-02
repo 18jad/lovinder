cf8ed1e88b3d86c6e71bf2ea539fc3e908e45d7a
Jad Yahya <jadoyahya@gmail.com>
2022-10-02 21:56:51 +0300

    [Controllers]: Added user register controller

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use Validator;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "password" => "required|min:6",
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'error' => $validator->errors()
+            ], 401);
+        } else {
+            $user = User::create(array_merge(
+                $validator->validated(),
+                ['password' => bcrypt($request->password)]
+            ));
+            return response()->json([
+                'status' => true,
+                'message' => 'User successfully registered',
+                'user' => $user
+            ], 201);
+        }
+    }
 }
