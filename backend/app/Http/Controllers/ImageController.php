48fafebdfb42ed873c258fa0db902197fe81ae12
Jad Yahya <jadoyahya@gmail.com>
2022-10-03 23:55:10 +0300

    [Controller]: Fixed image updating and improved image name

--- backend/app/Http/Controllers/ImageController.php
+++ backend/app/Http/Controllers/ImageController.php
@@ -6,9 +6,14 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Validator;
 
+use App\Models\User;
+
 class ImageController extends Controller
 {
-
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
 
     public function upload(Request $request)
     {
@@ -21,10 +26,27 @@ class ImageController extends Controller
                 'error' => $validator->errors()
             ], 401);
         } else {
-            $imageName = time() . auth()->id() . '.' . $request->image->extension();
+            // get the image name with the file type
+            $uploaded_image_full_name = $request->image->getClientOriginalName();
+
+            // extract only image name without extension type
+            $uploaded_image_name = pathinfo($uploaded_image_full_name, PATHINFO_FILENAME);
+
+            // rename the image to image original name + current tiem + user id + .image type
+            $imageName = $uploaded_image_name . time() . auth()->user()->id . '.' . $request->image->extension();
+
+            // store the image inside iamges folder in public directory (public/images)
             $request->image->move(public_path('images'), $imageName);
+
+            // update current logged in user field
+            User::where('id', auth()->user()->id)->update([
+                'profile' => $imageName
+            ]);
             return response()->json([
                 "status" => true,
+                "image_name" => $imageName,
+                "image_path" => public_path('images'),
+                "image_src" => "/backend/public/images/" . $imageName,
             ]);
         }
     }
