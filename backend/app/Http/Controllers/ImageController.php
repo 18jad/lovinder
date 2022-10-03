c2b1fd24ed424d2e18a14c6b59962adfbafe80c4
Jad Yahya <jadoyahya@gmail.com>
2022-10-03 22:39:28 +0300

    [Controller]: Fixed a bug in image upload controller

--- backend/app/Http/Controllers/ImageController.php
+++ backend/app/Http/Controllers/ImageController.php
@@ -6,9 +6,9 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Validator;
 
-
 class ImageController extends Controller
 {
+
 
     public function upload(Request $request)
     {
@@ -21,7 +21,7 @@ class ImageController extends Controller
                 'error' => $validator->errors()
             ], 401);
         } else {
-            $imageName = time() . auth()->user()->name . '.' . $request->image->extension();
+            $imageName = time() . auth()->id() . '.' . $request->image->extension();
             $request->image->move(public_path('images'), $imageName);
             return response()->json([
                 "status" => true,
