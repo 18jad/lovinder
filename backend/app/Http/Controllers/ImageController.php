62b9320693e6943a9b76d7beb3767a20a62cb9f5
Jad Yahya <jadoyahya@gmail.com>
2022-10-03 15:59:17 +0300

    [Controller]: Added image controller

---
+++ backend/app/Http/Controllers/ImageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Validator;
+
+
+class ImageController extends Controller
+{
+
+    public function upload(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:png,jpg,jpeg'
+        ]);
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'error' => $validator->errors()
+            ], 401);
+        } else {
+            $imageName = time() . auth()->user()->name . '.' . $request->image->extension();
+            $request->image->move(public_path('images'), $imageName);
+            return response()->json([
+                "status" => true,
+            ]);
+        }
+    }
+}
