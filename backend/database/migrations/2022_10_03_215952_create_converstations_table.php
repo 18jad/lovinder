d64d4df0e90a42dcc8e086436dc7580f3da849a3
Jad Yahya <jadoyahya@gmail.com>
2022-10-04 12:23:56 +0300

    [Migration]: Adjusted conversation name typo

--- backend/database/migrations/2022_10_03_215952_create_converstations_table.php
+++ backend/database/migrations/2022_10_03_215952_create_converstations_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('converstations', function (Blueprint $table) {
+        Schema::create('conversations', function (Blueprint $table) {
             $table->id();
             $table->string('user_id');
             $table->string('converstation_with');
