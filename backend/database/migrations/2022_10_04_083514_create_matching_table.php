c49c4b0ea71bbc39df6edf4cf7c65a4644c70268
Jad Yahya <jadoyahya@gmail.com>
2022-10-04 12:23:38 +0300

    [Migration]: Adjusted matching table

--- backend/database/migrations/2022_10_04_083514_create_matching_table.php
+++ backend/database/migrations/2022_10_04_083514_create_matching_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('matching', function (Blueprint $table) {
+        Schema::create('matchings', function (Blueprint $table) {
             $table->id();
             $table->integer("first_id");
             $table->integer("second_id");
