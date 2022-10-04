cc722c57f7d3389dca73673ec1262d3b9876bf14
Jad Yahya <jadoyahya@gmail.com>
2022-10-04 12:26:48 +0300

    [Controller]: Created check match function to check if 2 users match

--- backend/app/Http/Controllers/MatchingController.php
+++ backend/app/Http/Controllers/MatchingController.php
@@ -33,10 +33,23 @@ class MatchingController extends Controller
             Matching::insert([
                 ['first_id' => $user_id, 'second_id' => $match_with_id],
             ]);
-            // $this->checkMatch($user_id, $match_with_id);
+            $this->checkMatch($user_id, $match_with_id);
             return response()->json([
                 'status' => true,
                 'message' => 'Successfully matched'
+            ]);
+        }
+    }
+
+    // to check if 2 users match if yes create a conversation for them
+    public function checkMatch($user_id, $match_with_id)
+    {
+        $query1 = Matching::where('first_id', $user_id)->where('second_id', $match_with_id)->first();
+        $query2 = Matching::where('first_id', $match_with_id)->where('second_id', $user_id)->first();
+        if ($query1 && $query2) {
+            Conversation::insert([
+                ['user_id' => $user_id, 'converstation_with' => $match_with_id],
+                ['user_id' => $match_with_id, 'converstation_with' => $user_id],
             ]);
         }
     }
