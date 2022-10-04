599a00657a124260b17c91df84e9e15d0ab2aa53
Jad Yahya <jadoyahya@gmail.com>
2022-10-04 22:43:22 +0300

    [Controller]: Added functions from info controller

--- backend/app/Http/Controllers/MessagesController.php
+++ backend/app/Http/Controllers/MessagesController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Conversation;
+use App\Models\Message;
+use App\Models\User;
+
 
 class MessagesController extends Controller
 {
-    //
+    public function fetchChat(Request $request)
+    {
+        $chats = Conversation::all()->where('user_id', $request->user()->id)->unique(['converstation_with']);
+        $result = [];
+        foreach ($chats as $chat) {
+            $result[] = $this->fetchUserById_no_request($chat->converstation_with);
+        }
+        return response()->json($result);
+    }
+
+    public function fetchMessages(Request $request)
+    {
+        $conversation_id = $request->conversation_id;
+        $messages = Message::all()->where('converstation_id', $conversation_id);
+        return response()->json($messages);
+    }
+
+    public function fetchUserById_no_request($id)
+    {
+        $user = User::find($id);
+        return response()->json($user);
+    }
 }
